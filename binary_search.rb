e724d73eee201c80d9b71245b0a2fa47761d5261
tongoonamujera <tongoonamujera@gmail.com>
2021-10-29 20:48:25 +0200

    Implement binary_search

--- binary_search.rb
+++ binary_search.rb
@@ -1,22 +1,25 @@
-def binary_search(arr, key)
-  return nil if arr.empty?
-  
-  lower_bount, upper_bount = 0, arr.size - 1
-  midpoint = (upper_bount) / 2
-  while lower_bount < upper_bount
-    if arr[midpoint] == key
-      true
-    elsif arr[midpoint] < key
-      lower_bount = midpoint
-      midpoint = (upper_bount + lower_bount) / 2
-    else
-      upper_bount = midpoint - 1
-      midpoint = (upper_bount + lower_bount) / 2
+module Binary_search
+  def binary_search(arr, key)
+    
+    lower_bount, upper_bount = 0, arr.size - 1
+    midpoint = upper_bount / 2
+    while lower_bount < upper_bount
+      if arr[midpoint] == key
+        true
+      elsif arr[midpoint] < key
+        lower_bount = midpoint
+        midpoint = upper_bount + lower_bount / 2
+      else
+        upper_bount = midpoint - 1
+        midpoint = upper_bount + lower_bount / 2
+      end
     end
+    false
   end
-  false
 end
+
+include Binary_search
 
 arr = [15,16,17,18,19]
 
-puts binary_search(arr, 15)
+puts binary_search(arr, 17)
